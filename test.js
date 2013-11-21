0f02d39c8f24cd4ecf7e6243cd1fe94cb405de81
nickarthur <metrobro2004@gmail.com>
2013-11-21 10:45:28 -0600

    test spaces

--- test.js
+++ test.js
@@ -25,3 +25,33 @@ function sayHello() {
 function alert(){
   say('Hello');
 }
+
+
+function doDatShizt() {
+    sayHello();
+    sayHello();
+
+    init();
+}
+
+function bubba() {
+
+}
+
+handlerCreate({ '', '', '' });
+
+{{id:'hi',name:'ho'}},
+{{id:'hi',name:'ho'}},
+{{id:'hi',name:'ho'}},
+{{id:'hi',name:'ho'}},
+{{id:'hi',name:'ho'}};
+
+function abba() {
+  var a = bbb;
+}
+
+function ccc(){
+  //removed the space before opening bracket
+}
+
+
