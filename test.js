842cd086a08bb8ee3e554b88fdba1d2005868109
nickarthur <metrobro2004@gmail.com>
2013-11-21 09:29:48 -0600

    added some code

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
-﻿aaa
-bbb
-ccc
-ddd
+﻿var aaa;
+var bbb;
+var ccc;
+var ddd;
+
+
+function init() {
+    //this is a test
+    aaa = 0;
+    bbb = 5;
+    ccc = 7;
+    ddd = 9;
+
+    alert(aaa + bbb);
+
+}
